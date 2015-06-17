d21a47a6bceffb6df0e6e2e5c1f7afd39a00b610
Gaz Liddon MBR13 <gaz@gazaxian.com>
2015-06-17 05:21:09 +0100

    WIP

--- src/cljs/cloj/web/utils.cljs
+++ src/cljs/cloj/web/utils.cljs
@@ -4,7 +4,7 @@
     [hipo.core              :as hipo  :include-macros true]
     [dommy.core             :as dommy :include-macros true]  ))
 
-(defn id-ize [v] (str "#" v))
-(defn by-id [v] (-> (id-ize v) (dommy/sel1)))
+(defn id-ize   [v] (str "#" v))
+(defn by-id    [v] (-> (id-ize v) (dommy/sel1)))
 (defn get-dims [e] (mapv e [:width :height]))
-(defn log-js [v] (.log js/console v))
+(defn log-js   [v] (.log js/console v))
