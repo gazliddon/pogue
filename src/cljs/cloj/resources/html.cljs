b840256a014b38b6f16c6cc2e97287c635670457
Gaz Liddon MBR13 <gaz@gazaxian.com>
2015-06-17 15:36:51 +0100

    mergan

--- src/cljs/cloj/resources/html.cljs
+++ src/cljs/cloj/resources/html.cljs
@@ -3,16 +3,7 @@
 
   (:require [cloj.resources.manager :as rman]
             [cloj.render.canvas     :as canvas-render]
-<<<<<<< HEAD
-            [cljs.core.async        :refer [put! >! chan <! alts! close!]]
-            [cloj.render.protocols  :as rp]
-            [cljs-http.client       :as http]
-            [hipo.core              :as hipo  :include-macros true]
-            [dommy.core             :as dommy :include-macros true]))
-
-(defn id-ize [v] (str "#" v))
-(defn by-id [v] (-> (id-ize v) (dommy/sel1)))
-=======
+            ))
             [cloj.web.utils         :refer [by-id get-dims]]
             [cljs.core.async        :refer [put! >! chan <! alts! close!]]
             [cloj.render.protocols  :as rp]
@@ -20,7 +11,6 @@
             [dommy.core             :as dommy :include-macros true]    
             [hipo.core              :as hipo  :include-macros true]))
 
->>>>>>> 51c141b3a1b88e4e618dcb284d0fbe24d5162508
 
 (defprotocol ICanvasImage
   (img [_]))
@@ -34,11 +24,6 @@
 (defn create-data-img [id img-data]
   (hipo/create [:img ^:attrs {:id id :src img-data}] ))
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 51c141b3a1b88e4e618dcb284d0fbe24d5162508
 (defn mk-resource-manager [dom-div-id]
   (let [store (atom {:imgs [] :targets []})
         dom-div (by-id dom-div-id) ]
@@ -46,12 +31,7 @@
       rman/IResourceManagerInfo
       (find-img [_ id]
         (let [img (by-id id)
-<<<<<<< HEAD
-              w         (dommy/px img :width)
-              h         (dommy/px img :height) ]
-=======
-              [w h] get-dims ]
->>>>>>> 51c141b3a1b88e4e618dcb284d0fbe24d5162508
+              [w h] get-dims  ]
           (reify
             rman/IImage (width [_]  w) (height [_] h)
             ICanvasImage (img [_] img))))
