144d73bb6019f149e0559a911d2aee86f93a837d
Gaz Liddon MBR13 <gaz@gazaxian.com>
2015-06-21 13:37:18 +0100

    Removed bilinear scaling
    Ugghh!

--- src/cljs/cloj/render/canvas.cljs
+++ src/cljs/cloj/render/canvas.cljs
@@ -13,60 +13,69 @@
   (get-element [_])
   (get-ctx-2d [_]))
 
+
+(defn set-smoothing [ctx v]
+  (doto ctx
+        (aset "mozImageSmoothingEnabled"     v)
+        (aset "webkitImageSmoothingEnabled"  v)
+        (aset "msImageSmoothingEnabled"      v)
+        (aset "imageSmoothingEnabled"        v)))
+
 (defn canvas [canvas-id {:keys [x y] :as dims}]
   (let [canvas-el (hipo/create [:canvas ^:attrs {:id canvas-id :width x :height y}])
         ctx (.getContext canvas-el "2d")]
-    (reify
-      IHTMLCanvas
-      (get-element [_] canvas-el)
-      (get-ctx-2d [_]  ctx)
+    (do
+      (set-smoothing ctx false)
+      (reify
+        IHTMLCanvas
+        (get-element [_] canvas-el)
+        (get-ctx-2d  [_] ctx)
 
-      rp/ITransformable
-      (matrix! [this [a c e b d f]]
-        (do
-          (.setTransform ctx a b c d e f))
-        this)
+        rp/ITransformable
+        (matrix! [this [a c e b d f]]
+          (do
+            (.setTransform ctx a b c d e f))
+          this)
 
-      (identity! [this]
-        (do
-          (.resetTransform ctx))
-        this)
+        (identity! [this]
+          (do
+            (.resetTransform ctx))
+          this)
 
-      (translate! [this [ {:keys [x y]}]]
-        (do
-          (.translate ctx x y))
-        this)
+        (translate! [this [ {:keys [x y]}]]
+          (do
+            (.translate ctx x y))
+          this)
 
-      (scale! [this [ {:keys [x y] } ]]
-        (do
-          (.scale ctx x y))
-        this)
+        (scale! [this [ {:keys [x y] } ]]
+          (do
+            (.scale ctx x y))
+          this)
 
-      (rotate! [this v]
-        (do 
-          (.rotate ctx v))
-        this)
+        (rotate! [this v]
+          (do 
+            (.rotate ctx v))
+          this)
 
-      rp/IRenderBackend
+        rp/IRenderBackend
+        (spr-scaled! [this spr {x :x y :y} {w :x h :y}]
+          (do 
+            (let [[sx sy sw sh] (rman/dims spr)
+                  spr-img (rman/img spr) ]
+              (.drawImage ctx spr-img sx sy sw sh x y w h)))
+          this)
 
-      (spr-scaled! [this spr {x :x y :y} {w :x h :y}]
-        (do 
-          (let [[sx sy sw sh] (rman/dims spr)
-                spr-img (rman/img spr) ]
-            (.drawImage ctx spr-img sx sy sw sh x y w h)))
-        this)
+        (spr! [this spr pos]
+          (let [[sx sy sw sh] (rman/dims spr)]
+            (rp/spr-scaled! this spr pos (v2 sw sh) )))
 
-      (spr! [this spr pos]
-        (let [[sx sy sw sh] (rman/dims spr)]
-          (rp/spr-scaled! this spr pos (v2 sw sh) )))
+        (clear! [this col]
+          (rp/box! this (v2 0 0) dims col))
 
-      (clear! [this col]
-        (rp/box! this (v2 0 0) dims col))
-
-      (box! [this {x :x y :y} {w :x h :y} col]
-        (do
-          (let [col-str (rgb-str col)]
-            (set! (.-fillStyle ctx) col-str)
-            (.fillRect ctx x y w h)))))  )
+        (box! [this {x :x y :y} {w :x h :y} col]
+          (do
+            (let [col-str (rgb-str col)]
+              (set! (.-fillStyle ctx) col-str)
+              (.fillRect ctx x y w h))))))  )
 
   )
