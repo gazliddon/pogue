62a790253af66344cf81d18a8e6b447b4f985edb
Gaz Liddon FatGaz <gaz@gazaxian.com>
2015-06-29 06:26:23 +0100

    Map difference util
    Will use on keys

--- src/cljs/cloj/utils.cljs
+++ src/cljs/cloj/utils.cljs
@@ -1,5 +1,6 @@
 (ns cloj.utils
   (:require
+    [clojure.set :refer [difference]]
     [goog.string :as gstring]
     [goog.string.format :as gformat])
   )
@@ -7,4 +8,19 @@
 ;; TODO: Should be in a cljs specfic lib
 
 (defn format [ arg & args] (apply gstring/format arg args) )
+
+(defn map-difference
+  [orig other]
+  (let [changed (difference (set orig) (set other))
+        added (difference (set (keys other)) (set (keys orig))) ]
+
+    {:changed (->
+                (fn [r [k v] ]
+                  (assoc r k {:old v :new (k other)}))
+                (reduce {} changed))
+     :added (->>
+              (map (fn [k] [k (k other)]) added) 
+              (into {}))
+     }))
+
 
