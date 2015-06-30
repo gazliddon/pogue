e1f499f751415fac7e67507b8a1c342c869a3c50
Gaz Liddon FatGaz <gaz@gazaxian.com>
2015-06-30 06:43:05 +0100

    Added a few functions
    fract, sin, sin-01

--- src/cljs/cloj/math/misc.cljs
+++ src/cljs/cloj/math/misc.cljs
@@ -1,13 +1,27 @@
 (ns cloj.math.misc)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Proxies for js - needs to be in own file
 (defn log [v] (Math/log v))
 (defn cos [^float v] (Math/cos v))
+(defn sin [^float v] (Math/sin v))
 
 (defn ceil [v] (Math/ceil v))
 (defn floor [v] (Math/floor v))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(defn fract "doc-string" [v] (- v (floor v)))
+
 (defn clamp [l h v] (min h (max v l)))
+
+(defn sin-01 [^float v] (/ (+ 1 (sin v)) 2))
 (defn cos-01 [^float v] (/ (+ 1 (cos v)) 2))
+
+
 (defn in-range? [lo hi v] (= v (clamp lo hi v)))
 (defn clamp01 [^float v] (min 1 (max v 0)))
 (defn to-255 [^float v] (int (* 255 (clamp01 v)))  )
