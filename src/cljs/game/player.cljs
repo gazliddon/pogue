6f8e3e9ac66583cd8aacacaf198b850d75349c93
Gaz Liddon FatGaz <gaz@gazaxian.com>
2015-06-30 06:43:22 +0100

    WIP

--- src/cljs/game/player.cljs
+++ src/cljs/game/player.cljs
@@ -1,8 +1,58 @@
 (ns game.player
-  (:require
+  (                         :require
     [cloj.math.vec2         :as v2 :refer [vec2 vec2-s]]
     [cloj.utils             :refer [format map-difference] ]
+    [cloj.math.misc         :refer [sin cos fract]]
     ))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defprotocol IOscillator
+  (get-v [this t]))
+
+(defn square-wave [t]
+  (if (> 0.5 (fract t))
+    -1
+    1))
+
+;; Assuming func returns -1 1
+(defrecord Oscillator [start-t phase amplitude freq func]
+  IOscillator
+  (get-v [_ t]
+    (let [dt (- t start-t)
+          t' (+ phase  (/ dt freq))
+          v  (func t') ]
+      (* amplitude v))))
+
+(defmulti mk-osc (fn [kind _ _ _ _] kind))
+
+(defmethod mk-osc :sin [start-t phase amplitude freq ]
+  (->Oscillator start-t phase amplitude freq sin))
+
+(defmethod mk-osc :cos [start-t phase amplitude freq ]
+  (->Oscillator start-t phase amplitude freq cos))
+
+(defmethod mk-osc :default [start-t phase amplitude freq ]
+  (throw "not implemented"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defprotocol IMovement
+  (get-p-at-t [this t])
+  (get-v-at-t [this t]))
+
+(defn mk-sin-movement [start-t pos y-scale x-scale]
+  (reify
+    IMovement
+
+    (get-p-at-t [this t]
+      )
+
+    (get-v-at-t [this t]) 
+    )
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defprotocol IPlayer
   (get-pos [this t])
@@ -14,8 +64,8 @@
     )
   )
 
-(defn mk-player [t p]
-  (->Player (->EaserV2 t p v2/zero v2/zero)))
+; (defn mk-player [t p]
+;   (->Player (->EaserV2 t p v2/zero v2/zero)))
 
 
 ; (def old-map {:proc-1 6502
