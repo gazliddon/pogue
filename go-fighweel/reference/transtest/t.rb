6d879492b81e894be4f3f649f01b3b3b571cbdfd
Gaz Liddon FatGaz <gaz@gazaxian.com>
2015-07-25 09:03:14 +0100

    DSL kind of working nice :)

--- go-fighweel/reference/transtest/t.rb
+++ go-fighweel/reference/transtest/t.rb
@@ -1,26 +1,25 @@
 require 'transit'
+require 'rmath3d/rmath3d'
+require 'pp'
+
+include RMath3D
 
 io = StringIO.new('', 'w+')
 writer = Transit::Writer.new(:json, io)
 
-# Vert is x y u v, all floats
-def vert x,y,u,v
-    [x, y, u, v]
+
+def to_v3 v
+    RVec3.new(*v)
 end
 
-def verts v
-    return {:vals => v.flatten.each {|vv| vv.to_f},
-            :num => v.length }
-end
+quad_verts = [ [0,0,1],
+               [1,0,0],
+               [1,1,0],
+               [0,1,0], ].map {|vv| to_v3 vv}
 
-quad = {:indicies => [0,3,1,2],
-        :verts => verts( [vert(0,0,0,0), 
-                          vert(1,0,1,0),
-                          vert(1,1,1,1),
-                          vert(0,1,0,1)] )}
+pp quad_verts
 
-data = {:models => {:quad => quad}}
-
+data = {}
 
 writer.write(data)
 puts io.string
