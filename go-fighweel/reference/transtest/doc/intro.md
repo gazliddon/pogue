6901d38650c0201c64c43b992c503bb0840f0bb0
Gaz Liddon FatGaz <gaz@gazaxian.com>
2015-07-25 09:22:28 +0100

    Turning this from a clojure to ruby tool

--- go-fighweel/reference/transtest/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to transtest
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- go-fighweel/reference/transtest/test/transtest/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns transtest.core-test
-  (:require [clojure.test :refer :all]
-            [transtest.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
